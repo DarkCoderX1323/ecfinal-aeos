b294cd540c7460fb5a725308a429e84cb537c7b7
DarkCoder1323 <rayvargas1323@gmail.com>
2023-08-20 13:09:15 -0500

    Update

--- README.md
+++
@@ -1,2 +0,0 @@
-# ecfinal-dsw1
-Evaluación Final del curso de Desarrollo de Servicios Web 1
--- ecfinal/src/main/java/com/example/ecfinal/TrabajadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecfinal;
-
-import org.springframework.data.annotation.RepositoryRestResource;
-import org.springframework.data.repository.CrudRepository;
-
-@RepositoryRestResource(collectionResourceRel = "trabajadores", path = "trabajadores")
-public interface TrabajadorRepository extends CrudRepository<Trabajador, Long> {
-
-}
