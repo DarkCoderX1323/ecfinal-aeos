47e7abfb02dec1b76b6ef98712fd2b35363dcbb2
DarkCoder1323 <rayvargas1323@gmail.com>
2023-08-21 09:08:46 -0500

    Update

--- ecfinal.sql
+++ ecfinal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-08-2023 a las 06:31:14
+-- Tiempo de generación: 21-08-2023 a las 16:07:45
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -45,6 +45,30 @@ INSERT INTO `empresa` (`id`, `nombre`, `razon_social`, `rubro`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `marca`
+--
+
+CREATE TABLE `marca` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `origen` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `marca`
+--
+
+INSERT INTO `marca` (`id`, `nombre`, `origen`) VALUES
+(1, 'Toyota', 'Japón'),
+(2, 'Ford', 'Estados Unidos'),
+(3, 'Chevrolet', 'Estados Unidos'),
+(7, 'Tesla', 'Estados Unidos'),
+(8, 'Audi', 'Europa'),
+(9, 'Lancia', 'Europa');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `persona`
 --
 
@@ -66,6 +90,33 @@ INSERT INTO `persona` (`id`, `nombre`, `dni`, `sexo`, `edad`, `numero`) VALUES
 (3, 'Ray Vargas', '76534813', 'Masculino', 21, '965767744'),
 (4, 'Nick Julcarima', '98765432', 'Masculino', 21, '943852728');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehiculo`
+--
+
+CREATE TABLE `vehiculo` (
+  `id` bigint(20) NOT NULL,
+  `modelo` varchar(255) NOT NULL,
+  `año` int(11) DEFAULT NULL,
+  `precio` double NOT NULL,
+  `marca_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `vehiculo`
+--
+
+INSERT INTO `vehiculo` (`id`, `modelo`, `año`, `precio`, `marca_id`) VALUES
+(1, 'Corolla', 2022, 25000, 1),
+(2, 'Mustang', 2021, 35000, 2),
+(3, 'Camaro', 2020, 32000, 3),
+(4, 'Yaris', 0, 22000, 1),
+(5, '2.0', 2023, 150000, 7),
+(6, 'R8 GT', 2012, 250000, 8),
+(7, 'Delta', 1965, 15000, 9);
+
 --
 -- Índices para tablas volcadas
 --
@@ -77,10 +128,23 @@ ALTER TABLE `empresa`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `marca`
+--
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `persona`
 --
 ALTER TABLE `persona`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `marca_id` (`marca_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -93,10 +157,32 @@ ALTER TABLE `empresa`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de la tabla `marca`
+--
+ALTER TABLE `marca`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT de la tabla `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD CONSTRAINT `vehiculo_ibfk_1` FOREIGN KEY (`marca_id`) REFERENCES `marca` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
