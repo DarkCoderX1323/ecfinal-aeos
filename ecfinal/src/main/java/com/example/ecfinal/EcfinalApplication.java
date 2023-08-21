a1e3c6cd54bed612ce42ffd1b25dc4762db06252
DarkCoder1323 <rayvargas1323@gmail.com>
2023-08-20 23:38:46 -0500

    Update

--- ecfinal/src/main/java/com/example/ecfinal/EcfinalApplication.java
+++ ecfinal/src/main/java/com/example/ecfinal/EcfinalApplication.java
@@ -19,7 +19,7 @@ public class EcfinalApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000") // Cambia a la URL de tu frontend
+                        .allowedOrigins("http://localhost:3000") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
